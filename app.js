1c60523596758dab30bd0db17bdae8e312b2d2a8
Alex Tuti <alextutovan@tutanota.com>
2019-03-21 23:12:34 +0100

    Structure ready/ Add movie works

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
 const container=document.querySelector('.container');
 const movieForm=document.getElementById('movie-form');
-const movieName=document.getElementById('movie-name');
-const movieCategory=document.getElementById('movie-category');
-const movieYear=document.getElementById('movie-year');
-const movieDirector=document.getElementById('movie-director');
+
+
 
 
 movieForm.addEventListener('submit', addMovie);
-
+// Movie class for handling input values
 class Movie{
-    constructor(movieName, movieCategory, movieYear, movieDirector){
-        this.movieName = movieName.value;
-        this.movieCategory = movieCategory.value;
-        this.movieYear = movieYear.value;
-        this.movieDirector = movieDirector.value;
+    constructor(name, category, year, director){
+        this.name = name;
+        this.category = category;
+        this.year = year;
+        this.director = director;
     }
 }
 
+// UI class for manipulating the DOM
+class UI{
+    addMovieUI(movieData){
+        const movieTable=document.getElementById('movie-table-body');
+
+        const tableRow=document.createElement('tr');
+        tableRow.id = 'table-row';
+        tableRow.innerHTML = `
+            <td>${movieData.name}</td>
+            <td>${movieData.category}</td>
+            <td>${movieData.year}</td>
+            <td>${movieData.director}</td>
+            <td><a id="delete-btn" href="#">Delete</a>
+        `;
+        movieTable.appendChild(tableRow);
+    }
+
+    clearInput(){
+        document.getElementById('movie-name').value='';
+        document.getElementById('movie-category').value='';
+        document.getElementById('movie-year').value='';
+        document.getElementById('movie-director').value='';
+    }
+}
+
+// Function for adding movies to the list
 function addMovie(e){
     
-    const movieNameOutput=document.createElement('p');
-    movieNameOutput.textContent = movieData.movieName;
-    const movieCategoryOutput=document.createElement('p');
-    movieCategoryOutput.textContent = movieData.movieCategory;
-    const movieYearOutput=document.createElement('p');
-    movieYearOutput.textContent = movieData.movieYear;
-    const movieDirectorOutput = document.createElement('p');
-    movieDirectorOutput.textContent = movieData.movieDirector;
 
-    container.appendChild(movieNameOutput);
+    const name=document.getElementById('movie-name').value;
+    const category=document.getElementById('movie-category').value;
+    const year=document.getElementById('movie-year').value;
+    const director=document.getElementById('movie-director').value;
+    
+    const movieData = new Movie(name, category, year, director);
+    
+    // Validation of the input
+    
 
+    const ui = new UI();
+    ui.addMovieUI(movieData);
+    ui.clearInput();
     e.preventDefault();
 }
